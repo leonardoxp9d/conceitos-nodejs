e612adf931d7678cdc36371362f9c2cc4a04031e
Leonardo Soares <leonardoxp9d@gmail.com>
2020-08-18 08:53:31 -0300

    Projects CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const { uuid } = require("uuidv4");
+
 // const { uuid } = require("uuidv4");
 
 const app = express();
@@ -11,15 +13,28 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { 
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -27,7 +42,18 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id == id);
+
+  if(!repository){
+    return response.status(400).send();
+  }
+  
+  repository.likes += 1;
+
+  return response.json(repository);
+
 });
 
 module.exports = app;
